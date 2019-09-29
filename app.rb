7d5625173a8fba4d762ea30c6690749caa81c3f1
Robinson Regalado <rregalado17@gmail.com>
2019-09-29 01:07:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ class App < Sinatra::Base
   post 'checkout' do 
     session[:item] = params[:item] #setting the params in the session 
     @session = session 
-    
-    
+    erb :checkout
   end
   
 end
